b06815717dfec77a9be345f58d5f9266f974213f
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-17 21:30:07 +0530

    02 and 03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let rev = ""
+    let stringLength = string.length
+    for(let i = 1 ; i <= stringLength  ; i++){
+         rev += string[stringLength-i]
+    }
+    return rev
 };
 
 // Do not edit below this line
