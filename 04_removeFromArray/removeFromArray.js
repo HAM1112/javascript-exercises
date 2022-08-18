7aeef28e29a299227954dacfae767a5a52a13288
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-18 07:13:26 +0530

    04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array ,...item /*item2*/) {
+    //let itemIndex = array.indexOf(item);
+    // let item2Index = array.indexOf(item2)
 
+    // if(itemIndex > -1){
+    //     array.splice(itemIndex,1)
+    // }
+    // if(item2Index > -1){
+    //     array.splice(item2Index,1)
+    // }
+    // return array;
+    item.forEach(item => {
+        let itemIndex = array.indexOf(item);
+        if(itemIndex >-1){
+            array.splice(itemIndex,1)
+        }
+        
+    });
+    return array;
 };
 
 // Do not edit below this line
