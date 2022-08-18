cdd285595cd9f92805b9b9b59e2af7879446abb3
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-18 08:39:35 +0530

    05 sum all completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num , num2) {
+    let sum = 0;
+    if(num < 0 || num2 < 0 || !Number.isInteger(num) || !Number.isInteger(num2)){
+        return "ERROR"
+    }
+    else {
+        if(num<num2){ 
+        for(let i = num ; i <= num2 ; i++){
+            sum += i;
+        }
+        }
+        else {
+            for(let i = num2 ; i <= num ; i++){
+                sum += i;
+            }
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
