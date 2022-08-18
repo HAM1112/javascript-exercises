79d028e35b751244ed20db7ede5ba561a8d379b8
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-18 21:14:02 +0530

    07 tempConversion and 08 calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1 , num2) {
+	let add = num1 + num2;
+  return add;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1 ,num2) {
+	let subtract = num1 - num2;
+  return subtract;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  let sum =0;
+  num.forEach(num => {
+      sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let multiple = 1;
+  nums.forEach(nums => {
+    multiple *= nums;
+  })
+  return multiple;
 };
 
-const power = function() {
-	
+const power = function(num1 , num2) {
+	let power = 1;
+    for(i = 0 ; i < num2 ; i++){
+        power *= num1; 
+    }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1 ;
+    for( i = 1; i <= num ; i++){
+        factorial *= i;
+    }
+  return factorial;
 };
 
 // Do not edit below this line
