402de161597f4899174327f0a70bf3b0859c421a
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-19 20:51:05 +0530

    09 palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let palindromes ;
+    let reverse = '';
+    let cleanString = string.replace(/[^a-zA-Z]/g,"");
+    let newStr = cleanString.toLowerCase();
+    let newStrLength = newStr.length;
+   
+        for(i = 0 ; i < newStrLength ; i++){
+            reverse += newStr[newStrLength -1-i]
+        }
+        
+        if(reverse === newStr){
+            palindromes = true
+        }
+        else{
+            palindromes = false  
+        }
+    return palindromes
 };
 
 // Do not edit below this line
