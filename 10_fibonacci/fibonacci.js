4fc56a19811177f988205468bac668ed3024a921
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-23 23:44:33 +0530

    new -v npm

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    let firstNum  = 0;
+    let secondNum = 1;
+    let finalNum = 0;
+    for(i = 0 ; 1 < num ; i++){
+        finalNum = secondNum;
+        secondNum = finalNum;
+        finalNum = firstNum + secondNum;
+    }
+    return finalNum;
+};  
 
 // Do not edit below this line
 module.exports = fibonacci;
